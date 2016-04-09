a84d6b096ae1e554280d8bb60e6fefcecc5b47cf
Ryan Currah <Ryan.Currah@cantire.com>
2016-04-09 19:07:06 -0400

    convert indents to spaces

--- salt_pipeline.groovy
+++ salt_pipeline.groovy
@@ -11,64 +11,64 @@ def env_vars = ["FORMULA_NAME=${env.FORMULA_NAME}"]
 
 stage 'Build'
     node() {
-		withEnv(env_vars) {
-			try {
-				checkout scm
+        withEnv(env_vars) {
+            try {
+                checkout scm
                 sh 'git rev-parse --verify HEAD > ../commit'
 
-				// Set GIT_COMMIT env variable
+                // Set GIT_COMMIT env variable
                 env.GIT_COMMIT = readFile 'commit'
                 env.GIT_COMMIT = env.GIT_COMMIT.trim()
                 echo "GIT_COMMIT: ${env.GIT_COMMIT}"
-			}
-			catch (err) {
-				echo "Caught: ${err}"
-				currentBuild.result = 'FAILURE'
-				error err.getMessage()
-			}
-		}
+            }
+            catch (err) {
+                echo "Caught: ${err}"
+                currentBuild.result = 'FAILURE'
+                error err.getMessage()
+            }
+        }
     }
 
 stage 'QA'
     node() {
-		withEnv(env_vars) {
-			try {
-				sh '''
-				echo 'running tests'
-				'''
+        withEnv(env_vars) {
+            try {
+                sh '''
+                echo 'running tests'
+                '''
 
-				if (env.BRANCH_NAME != 'master') {
-					currentBuild.result = 'SUCCESS'
-				}
-			}
-			catch (err) {
-				echo "Caught: ${err}"
-				currentBuild.result = 'FAILURE'
-				error err.getMessage()
-			}
-		}
+                if (env.BRANCH_NAME != 'master') {
+                    currentBuild.result = 'SUCCESS'
+                }
+            }
+            catch (err) {
+                echo "Caught: ${err}"
+                currentBuild.result = 'FAILURE'
+                error err.getMessage()
+            }
+        }
     }
 
 if (env.BRANCH_NAME == 'master') {
     stage name: 'Production', concurrency: 1
         node() {
             withEnv(env_vars) {
-				try {
-					sh '''
-					echo "Promoting Salt formula..."
-					#curl "" -o bump_version.sh
-					#sudo sh bump_version.sh -a install
-					#sh bump_version.sh -a bump -c "${BRANCH_NAME}" -b "patch" -d ${FORMULA_NAME}
-					echo "...Promotion complete"
-					'''
+                try {
+                    sh '''
+                    echo "Promoting Salt formula..."
+                    #curl "" -o bump_version.sh
+                    #sudo sh bump_version.sh -a install
+                    #sh bump_version.sh -a bump -c "${BRANCH_NAME}" -b "patch" -d ${FORMULA_NAME}
+                    echo "...Promotion complete"
+                    '''
 
-					currentBuild.result = 'SUCCESS'
-				}
-				catch (err) {
-					echo "Caught: ${err}"
-					currentBuild.result = 'FAILURE'
-					error err.getMessage()
-				}
+                    currentBuild.result = 'SUCCESS'
+                }
+                catch (err) {
+                    echo "Caught: ${err}"
+                    currentBuild.result = 'FAILURE'
+                    error err.getMessage()
+                }
             }
         }
 }
