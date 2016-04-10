18aa88e2752c42ad373474107fe6a470e0b96ff7
Ryan Currah <Ryan.Currah@cantire.com>
2016-04-10 13:57:43 -0400

    jenkisnfile

--- salt_pipeline.groovy
+++ salt_pipeline.groovy
@@ -38,8 +38,9 @@ stage 'Build'
             echo 'Running code analysis'
             echo 'flake8...'
             sh "${DOCKER_EXEC} flake8 ."
+
             echo 'shellcheck...'
-            sh "${DOCKER_EXEC} sh -c 'find . -name '*.sh' | while read line; do shellcheck $line; done'"
+
 
             // Setup testing environment
             sh "${DOCKER_EXEC} \\cp -r tests/minion /etc/salt/minion"
