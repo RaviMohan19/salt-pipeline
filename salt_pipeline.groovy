90a9cbee16b9f91d615d5eb297ea70f9dd2b4529
Ryan Currah <Ryan.Currah@cantire.com>
2016-04-09 22:11:34 -0400

    salt pipeline

--- salt_pipeline.groovy
+++ salt_pipeline.groovy
@@ -9,7 +9,7 @@ def JENKINS_GIT_CREDENTIAL_ID = 'f35a0dab-572d-43aa-a289-319ef3a3445d'
 // Set environment variables
 def env_vars = ["FORMULA_NAME=${env.FORMULA_NAME}"]
 
-// Setup container
+// Setup container, make sure docker user is root
 def DOCKER_PARAMS = "-u 0"
 def container = docker.image('ubuntu:14.04')
 
@@ -17,9 +17,6 @@ stage 'Build'
     node() {
         try {
             withEnv(env_vars) {
-                // Clear the workspace
-                deleteDir()
-
                 checkout scm
                 sh 'git rev-parse --verify HEAD > commit'
 
@@ -42,14 +39,13 @@ stage 'QA'
             container.inside(DOCKER_PARAMS) {
                 withEnv(env_vars) {
                     sh '''
-                    echo 'Install salt-minion'
-                    echo $(whoami)
+                    apt-get update
                     apt-get install -y curl
+                    apt-get install -y git
+                    apt-get install -y ruby2.0
+                    apt-get install -y python-pip
                     curl -o - "https://repo.saltstack.com/apt/ubuntu/$(lsb_release -sr)/amd64/latest/SALTSTACK-GPG-KEY.pub" | sudo apt-key add -
                     echo "deb http://repo.saltstack.com/apt/ubuntu/$(lsb_release -sr)/amd64/latest $(lsb_release -sc) main" > /etc/apt/sources.list.d/99-saltstack.list
-                    apt-get install -y git
-                    apt-get install -y python-pip
-                    apt-get install -y ruby2.0
                     apt-get install -y salt-minion
                     service salt-minion stop
                     '''
@@ -70,23 +66,22 @@ stage 'QA'
 if (env.BRANCH_NAME == 'master') {
     stage name: 'Production', concurrency: 1
         node() {
-            withEnv(env_vars) {
-                try {
-                    sh '''
-                    echo "Promoting Salt formula..."
-                    #curl "" -o bump_version.sh
-                    #sudo sh bump_version.sh -a install
-                    #sh bump_version.sh -a bump -c "${BRANCH_NAME}" -b "patch" -d ${FORMULA_NAME}
-                    echo "...Promotion complete"
-                    '''
+            try {
+                container.inside(DOCKER_PARAMS) {
+                    withEnv(env_vars) {
+                        sh '''
+                        echo "Promoting Salt formula..."
 
-                    currentBuild.result = 'SUCCESS'
+                        echo "...Promotion complete"
+                        '''
+                        currentBuild.result = 'SUCCESS'
+                    }
                 }
-                catch (err) {
-                    echo "Caught: ${err}"
-                    currentBuild.result = 'FAILURE'
-                    error err.getMessage()
-                }
+            }
+            catch (err) {
+                echo "Caught: ${err}"
+                currentBuild.result = 'FAILURE'
+                error err.getMessage()
             }
         }
 }
